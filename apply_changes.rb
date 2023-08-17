b207d1b58350e2018c1543b8518d9232c2428504
John Huynh <john@huynh.io>
2023-08-17 14:54:41 -0500

    Add initial CLI args processing

--- apply_changes.rb
+++ apply_changes.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby
+
+if ARGV.count < 3
+  puts "ERROR: not enough arguments"
+  puts "Usage: ./apply_changes.rb <input.json> <changes.json> <output.json>"
+end
+
+input_file = ARGV[0]
+changes_file = ARGV[1]
+output_file = ARGV[2]
+
+puts "Input file: #{input_file}"
+puts "Changes file: #{changes_file}"
+puts "Output file: #{output_file}"
